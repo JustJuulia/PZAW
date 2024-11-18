ca297b6050abbd7d111a46db12d36bc079803d29
Julia <102248096+JustJuulia@users.noreply.github.com>
2024-11-18 16:07:32 +0100

    added task from 18/11/2024
    new component didnt work so ichickened out and did it in some old one

--- pzaw_20240103/project_backend/index.js
+++ pzaw_20240103/project_backend/index.js
@@ -7,11 +7,20 @@ application.put("/mojput", (request, response) => {response.send('<a>asdadaddddd
 const sci_router = express.Router(
     
 )
+var cors = require('cors');
+application.use(cors());
+
 sci_router.get("/", (request, response) => {response.send("sci")})
 
 sci_router.get("/json", (request, response) => {
     const data = [{k1:"tekst", k2:2137}]
     response.json(data)})
+application.use(express.json())
+application.post("/newjson", (request, response) => {
+    const data = request;
+    console.log(data.body);
+    response.send();
+});
 application.use("/sci/4c/abc", sci_router)
 
 
--- pzaw_20240103/project_react/src/AxiosExample.js
+++ pzaw_20240103/project_react/src/AxiosExample.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function AxiosExample(){
     const [val, setVal] = useState();
     const funct_click = () => {
@@ -14,11 +15,37 @@ function AxiosExample(){
             console.log('finally')
         })
     }
+    const [imie, setImie] = useState('')
+    const [nazwisko, setNazwisko] = useState('')
+    const [haslo, setHaslo] = useState('')
+    const [check1, setCheck1] = useState('')
+    const [check2, setCheck2] = useState('')
+    const [gender, setGender] = useState(false)
+
+    const senddata = () => {
+        const json = JSON.parse('{"imie":"'+imie+'", "nazwisko":"'+nazwisko+'", "haslo":"'+haslo+'", "check1":"'+check1+'", "check2":"'+check2+'","gender":"'+gender+'"}')
+        axios.post("http://localhost:8000/newjson", {
+            json
+        }
+    )
+    }
+
 return(<>
-// TODO:add checkboxes and all
+
 {val}
 <h1>adadsa</h1>
 <input type="button" onClick={funct_click} value={"asdad"}/>
+<div>
+    <br></br>
+<input type="text" class="form-control" placeholder="imie" value={imie} onChange={(e) => setImie(e.target.value)}></input> <br></br>
+<input type="text"  class="form-control" placeholder="nazwisko" value={nazwisko} onChange={(e) => setNazwisko(e.target.value)}></input> <br></br>
+<input type="password"  class="form-control" placeholder="haslo" value={haslo} onChange={(e) => setHaslo(e.target.value)}></input> <br></br>
+<input type="checkbox"  class="form-check-input" className="checkboxy" value={check1} checked={check1 === 'wybrany'} onChange={() => setCheck1('wybrany')}/> jakis check <br></br>
+<input type="checkbox"  class="form-check-input" className="checkboxy" value={check2}  checked={check2 === 'wybrany'} onChange={() => setCheck2('wybrany')}/> jakis drygi check <br></br>
+<input type="radio"  class="form-check-input" name="radiobuttony" value={gender}  checked={gender === 'mail'} onChange={() => setGender('mail')}/> mail <br></br>
+<input type="radio" class="form-check-input" name="radiobuttony" value={gender}  checked={gender === 'gmail'} onChange={() => setGender('gmail')}/> gmail <br></br>
+<input type="button" class="btn btn-primary" onClick={senddata} value="wyslij dane"></input>
+</div>
 </>)
 }
 export default AxiosExample;
